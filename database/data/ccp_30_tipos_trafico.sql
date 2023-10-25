5ecf75f7a0d777cbf8f337df5639a5cd67c89f03
Carlos C Soto <eclipxe13@gmail.com>
2023-10-25 15:12:32 -0600

    Automatic update 2023-10-25

--- database/data/ccp_30_tipos_trafico.sql
+++ database/data/ccp_30_tipos_trafico.sql
@@ -1,7 +1,7 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-INSERT INTO ccp_30_tipos_trafico VALUES('TT01','Tráfico local','2021-12-01','');
-INSERT INTO ccp_30_tipos_trafico VALUES('TT02','Tráfico interlineal remitido','2021-12-01','');
-INSERT INTO ccp_30_tipos_trafico VALUES('TT03','Tráfico interlineal recibido','2021-12-01','');
-INSERT INTO ccp_30_tipos_trafico VALUES('TT04','Tráfico interlineal en tránsito','2021-12-01','');
+INSERT INTO ccp_30_tipos_trafico VALUES('TT01','Tráfico local','2023-11-25','');
+INSERT INTO ccp_30_tipos_trafico VALUES('TT02','Tráfico interlineal remitido','2023-11-25','');
+INSERT INTO ccp_30_tipos_trafico VALUES('TT03','Tráfico interlineal recibido','2023-11-25','');
+INSERT INTO ccp_30_tipos_trafico VALUES('TT04','Tráfico interlineal en tránsito','2023-11-25','');
 COMMIT;
